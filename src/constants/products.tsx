741286c6f3ff143c42c0a7bf76e47869eadb20ba
shkhrtrxsh <shkhrtrxsh@gmail.com>
2025-05-08 10:25:06 +0530

    added new project

--- src/constants/products.tsx
+++ src/constants/products.tsx
@@ -1,6 +1,7 @@
 import sidefolioAceternity from "public/images/sidefolio-aceternity-2.png";
 import sidefolioAceternity2 from "public/images/sidefolio-aceternity-2.png";
 import devtinder from "public/images/Screenshot 2025-03-20 at 9.42.31 PM.png";
+import sensai from "public/images/sensai.png";
 
 export const products = [
   {
@@ -10,7 +11,7 @@ export const products = [
     thumbnail: devtinder,
     images: [devtinder],
     stack: ["Reactjs", "Tailwindcss", "MongoDB", "Nodejs", "Express"],
-    slug: "aceternity",
+    slug: "devtinder",
     content: (
       <div>
         <p>
@@ -22,6 +23,39 @@ export const products = [
           Implemented authentication, real-time interactions, and matchmaking
           logic, optimizing backend performance and database queries for
           scalability.
+        </p>{" "}
+      </div>
+    ),
+  },
+  {
+    href: "https://sensai-livid.vercel.app/",
+    title: "Sensai",
+    description: "An AI powered career guidance platform.",
+    thumbnail: sensai,
+    images: [sensai],
+    stack: [
+      "Nextjs",
+      "Tailwindcss",
+      "NeonDB",
+      "Ingress",
+      "Gemini AI",
+      "NextAuth",
+      "Prisma",
+      "Clerk",
+      "Shadcn UI",
+    ],
+    slug: "sensai",
+    content: (
+      <div>
+        <p>
+          Designed and developed a full-stack career guidance platform using
+          Next.js, Tailwindcss, NeonDB, Ingress, Gemini AI, NextAuth, React
+          Query, and Shadcn UI, ensuring a seamless user experience.
+        </p>
+        <p>
+          Implemented clerk oauth authentication, real-time AI data fetching
+          using Gemini API key, and career guidance logic, optimizing backend
+          performance and database queries for scalability.
         </p>{" "}
       </div>
     ),
